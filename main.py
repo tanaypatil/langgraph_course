8b9a42731a48f58afd57b3e70b92698630f75ffb
tanaypatil <tanaypatil197@gmail.com>
2025-09-27 19:38:33 +0530

    Added react graph

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-# This is a sample Python script.
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
+from langgraph.constants import START, END
+from langgraph.graph import StateGraph, MessagesState
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from nodes import run_agent_reasoning_engine, tool_node
+
+load_dotenv()
+
+AGENT_REASON = "agent_reason"
+ACT = "act"
+LAST = -1
+
+flow = StateGraph(MessagesState)
+
+flow.add_node(AGENT_REASON, run_agent_reasoning_engine)
+flow.add_node(ACT, tool_node)
+flow.set_entry_point(AGENT_REASON)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def should_continue(state: dict) -> str:
+    if not state["messages"][LAST].tool_calls:
+        return END
+    return ACT
+
+
+flow.add_edge(START, AGENT_REASON)
+flow.add_conditional_edges(
+    AGENT_REASON,
+    should_continue,
+    {
+        END: END,
+        ACT: ACT,
+    },
+)
+flow.add_edge(ACT, AGENT_REASON)
+
+app = flow.compile()
+app.get_graph().draw_mermaid_png(output_file_path="graph.png")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    res = app.invoke(
+        {
+            "messages": [
+                HumanMessage(
+                    content="what is the weather in Nagpur, Maharashtra, India? List it and then Triple it "
+                )
+            ]
+        }
+    )
+    print(res["messages"][LAST].content)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
